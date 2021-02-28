4e322fb4c42504dc527369af75125c3916d6d6dc
MCMetro <74629901+MCMetro@users.noreply.github.com>
2021-02-28 16:10:48 -0600

    broken readMovie

--- src/omdb/readMovie.java
+++ src/omdb/readMovie.java
@@ -1,38 +1,49 @@
 package omdb;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 
 public class readMovie {
-	
-public static void main(String[] args) {
-		
+
+	public static void main(String[] args) {
+
 		try {
 			// Establishing connection to database
-			Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/omdb" , "root" , "");
-			
+			Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/omdb", "root", "");
+
 			// statement creation
 			Statement myStat = myConn.createStatement();
-			
+
+			// SQL query creation
+			String sqlQuery = "SELECT movie_id, english_name, native_name, year_made"
+					+ "FROM movies";
+//					+ "LEFT JOIN movie_data md on m.movie_id = md.movie_id "
+//					+ "WHERE md.movie_id = '3'";
+
 			// SQL query execution
-			ResultSet myRs = myStat.executeQuery("select * from movies");
-			ResultSet myRs = myStat.executeQuery( "select * from movie data");
-			
+			ResultSet myRs = myStat.executeQuery(sqlQuery);
+			int movieId = myRs.getInt("movie_id");
+			System.out.println("movie_id: " + movieId);
+
 			// process the results
-			while(myRs.next()) {
-				System.out.println(myRs.getString("movie_id") + ", " + myRs.getString("native_name") + ", " + myRs.getString("english_name") + ", " + myRs.getString("year_made"));
-				System.out.println(myRs.getString("tag_line") + ", " + myRs.getString("movie_id") + ", " + myRs.getString("language") + ", " + myRs.getString("country")+ ", " + myRs.getString("genre")+ ", " + myRs.getString("plot"));
-				
-			}
+			System.out.println("Passed SQL query");
 			
-			//close the connection
+			while (myRs.next()) {
+				System.out.println(myRs.getInt("m.movie_id") + ", " + myRs.getString("m.native_name") + ", "
+						+ myRs.getString("m.english_name") + ", " + myRs.getInt("m.year_made"));
+//				System.out.println(myRs.getString("md.tag_line") + ", "
+//						+ myRs.getString("md.language") + ", " + myRs.getString("md.country") + ", " + myRs.getString("md.genre")
+//						+ ", " + myRs.getString("md.plot"));
+
+			}
+
+			// close the connection
 			myConn.close();
 		}
-		
+
 		catch (Exception exc) {
-			
+
 			exc.printStackTrace();
+		}
+	}
 
 }
