35b802cc42a903327addfb13d8104010c7cab8e4
MCMetro <74629901+MCMetro@users.noreply.github.com>
2021-02-28 15:33:49 -0600

    created updateMovie and createMovie methods in MovieDriver.java

--- src/omdb/MovieDriver.java
+++ src/omdb/MovieDriver.java
@@ -32,5 +32,71 @@ public class MovieDriver {
 
 
     }
+    
+    // TODO: Add variables for driver calls later
+    public void updateMovie() {
+		String sqlUpdate = "UPDATE movies "
+                + "SET english_name = ?, year_made = ? "
+                + "WHERE movie_id = ?";
+
+		//create connection to database
+		//PreparedStatement allows for repeated use of mysql statement if needed. 
+        try (Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/Mydb", "root", "");
+                PreparedStatement pstmt = conn.prepareStatement(sqlUpdate)) {
+
+            // prepare data for update
+            String movie_name = "test";
+            int id = 3;
+            int movie_year = 1933;
+            pstmt.setString(1, movie_name);
+            pstmt.setInt(2, movie_year);
+            pstmt.setInt(3, id);
+            
+            
+            //print out rows affected
+            int rowAffected = pstmt.executeUpdate();
+            System.out.println(String.format("Row affected %d", rowAffected));
+            
+            conn.close();
+            
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+    
+    
+    // TODO: Add variables for driver calls later
+    public void createMovie() {
+        try {
+            //STEP 1: Start Connection to the Database
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/omdb", "root", "");
+            System.out.println("Connected database successfully...");
+            Statement stmt = null;
+            //STEP 2: Execute a query
+            System.out.println("Inserting new movie into the movie omdb.movies...");
+            stmt = conn.createStatement();
+            //STEP 3: Insert Values
+            String sql = "INSERT INTO movies " +
+                    "VALUES (1, 'Tenet', 'Tenet', 2020)";
+            stmt.executeUpdate(sql);
+            System.out.println("Movie created successfully!");
+            //STEP 4: Close the connection
+            conn.close();
+            System.out.println("Goodbye!");
+            //STEP 5: Catch Errors
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+    // TODO: Add variables for driver calls later
+    public void readMovie() {
+    	
+    }
+    
+    // TODO: Add variables for driver calls later
+    public void deleteMovie() {
+    	
+    }
 
 }
