77cce3bfd1a2fc5684b230c0547b679da74b7329
mhussein29 <77996250+mhussein29@users.noreply.github.com>
2021-02-28 15:20:44 -0600

    added code to updateMovie.java

--- src/omdb/movieUpdate.java
+++ src/omdb/movieUpdate.java
@@ -1,11 +1,42 @@
 //CRUD operation for UPDATE Movie file
 package omdb;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class movieUpdate {
 	
-	public boolean updateMovie(int movie_id) {
+	public static void main(String[] args) {
+		
+		String sqlUpdate = "UPDATE movies "
+                + "SET english_name = ?, year_made = ? "
+                + "WHERE movie_id = ?";
 
-		return false;
+		//create connection to database
+		//PreparedStatement allows for repeated use of mysql statement if needed. 
+        try (Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/Mydb", "root", "Sroot123");
+                PreparedStatement pstmt = conn.prepareStatement(sqlUpdate)) {
+
+            // prepare data for update
+            String movie_name = "test";
+            int id = 3;
+            int movie_year = 1933;
+            pstmt.setString(1, movie_name);
+            pstmt.setInt(2, movie_year);
+            pstmt.setInt(3, id);
+            
+            
+            //print out rows affected
+            int rowAffected = pstmt.executeUpdate();
+            System.out.println(String.format("Row affected %d", rowAffected));
+            
+            conn.close();
+            
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        //jdbc update code source: https://www.mysqltutorial.org/mysql-jdbc-update/
 	}
-
 }
