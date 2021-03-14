fac09338a762137380dab47438e92c7cb2e34069
Aziz Moalim <43540550+azizmoalim@users.noreply.github.com>
2021-03-14 16:34:15 -0500

    Updated moviedriver

--- src/omdb/MovieDriver.java
+++ src/omdb/MovieDriver.java
@@ -1,6 +1,7 @@
 package omdb;
 
 import java.sql.*;
+import java.util.Scanner;
 
 public class MovieDriver {
 
@@ -193,23 +194,67 @@ public class MovieDriver {
 
 		// TODO: Case 1: Aziz | Maamoun
 		//check if movie does not exist and create if needed
-		if ((nativeNameResult == null) && (String.valueOf(yearMadeResult) == null)) {
+		
+		
+		
+		Scanner scan = new Scanner(System.in);
+try {
+			// STEP 0: Ask for movie details
+			System.out.println("\nCreate a new movie! \n");
+			System.out.print("Enter movie ID: ");
+			movieID = scan.nextInt();
+			System.out.print("Enter the movie's English Name: ");
+			scan.nextLine(); // Consume the \n from nextInt above.
+			englishName = scan.nextLine();
+			System.out.print("Enter the movie's Native Name: ");
+			nativeName = scan.nextLine();
+			System.out.print("Enter the year the movie was made: ");
+			yearMade = scan.nextInt();
 
+			// Print Test:
+
+			// System.out.println(movieID);
+			// System.out.println(englishName);
+			// System.out.println(nativeName);
+			// System.out.println(yearMade);
+
+			// STEP 1: Start Connection to the Database
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/omdb", "root", "");
+			System.out.println("Connected to database successfully...");
+			Statement stmt = null;
+			// STEP 2: Execute a query
+			System.out.println("Rnning sql query...");
+			stmt = conn.createStatement();
+			// STEP 3: Insert Values
+			String mysqlQuery = "SELECT * FROM `movies` where native_name = '" + nativeName + "' AND year_made = " + yearMade;;
+			ResultSet myResults = stmt.executeQuery(mysqlQuery);
+
+			if (myResults.next()) {
+
+				System.out.println("Error: Movie with native name " + nativeName + " and year " + yearMade + " is a duplicate. Please try again");
+			} else {
+
+				// STEP 4: Extract Necessary Data
+				System.out.println("Movie is not a duplicate......");
+				System.out.println("Creating.....");
+				MovieDriver.createMovie(movieID, nativeName, englishName, yearMade);
+
+			}
+
+			// STEP 6: Close the connection
+		conn.close();
+			scan.close();
+			// STEP 7: Catch Errors
+		} catch (Exception ex) {
+			ex.printStackTrace();
 		}
-
-		// TODO: Case 2: Aziz | Maamoun
-		//check to see if movie exists and ignore creation if so
-		if (moviesResults.next()) {
-
-			System.out.println("Error: Movie with native name " + nativeName + " and year " + yearMade
-					+ " is a duplicate. Please try again");
-		} else {
-
-			// STEP 4: Extract Necessary Data
-			System.out.println("Movie is not a duplicate......");
-			System.out.println("Creating.....");
-			createMovie(movieID, nativeName, englishName, yearMade);
-		}
+		
+		
+		
+		
+	
+		
+		
 		// TODO: Case 3: Mahad
 		//check if songsResults query is empty; title does not exist in songs table
 		if (!songsResults.next()) {
