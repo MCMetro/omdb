5b9bd4cd7eda7c7a1b6c1a8e53e0fc518349d0ba
mslayhi <79674328+mslayhi@users.noreply.github.com>
2021-04-04 12:42:32 -0500

    people Song created
    Line 20 has error and IDK why

--- src/omdb/SongPeople.java
+++ src/omdb/SongPeople.java
@@ -1,5 +1,43 @@
+//Maamoun
 package omdb;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
 public class SongPeople {
+	private int songID;
+	private int peopleID;
+	private String role;
+	
+	
+	public SongPeople(int songID, int peopleID, String role ) {
+		this.songID = songID;
+		this.peopleID = peopleID;
+		this.role = role;
+		try {
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/omdb", "root", "");
+			String sqlQuery = "INSERT INTO people_song VALUES (" + songID + ", " + peopleID + ", " + role ")";
+			Statement msStat2 = conn.createStatement();
+			msStat2.executeUpdate(sqlQuery);
+			conn.close();
+		} catch(Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+	//songID getter method
+	public int getSongID() {
+		return songID;
+	}
+	//peopleID getter method 
+	public int getPeopleID() {
+		return peopleID;
+	}
+	
+	//role getter method
+	public String getRole() {
+		return role;
+	}
+	
 
 }
